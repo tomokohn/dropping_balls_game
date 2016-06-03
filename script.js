00985f4db5daf45172e117d73dfb6f930e759917
tomer kohn <tomokohn@gmail.com>
2016-06-03 13:01:16 +0300

    bullet

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
  * Created by Tomer on 20/05/2016.
  */
 jQuery( function () {
+    // create and remove the the ball randomly
     var createball = function () {
         var diameter = Math.floor(Math.random() * (150 - 20 + 1)) + 20,
             posX = (Math.floor(Math.random()* parseInt($('.area').css('width'))))-150,
             color = 'red',
-            b = $('<div class="ball"></div>').css({
+            ball = $('<div class="ball"></div>').css({
                 position: 'absolute',
                 left: posX,
                 background : color,
@@ -14,22 +15,42 @@ jQuery( function () {
                 height : diameter,
                 borderRadius : '50%'
             });
-        b = b.animate({top:'100vh'},600);
-        return b;
+        ball = ball.animate({top:'90vh'},600, function(){
+            $(this).remove();
+        });
+        return ball;
     }
+
+    //create the shooter
+    var createbullet = function () {
+        var shots = shots || 0;
+        $('.yellow').click(function (e) {
+            shots +=1;
+            console.log('shoot: ' + shots);
+            var bullet = $('<div class="bullet">')
+                .css('top',e.clientY);
+            $('.area').append(bullet);
+            $(bullet).animate({right:'95vw'},500, function () {
+                $(this).remove();
+            })
+        })
+
+
+    }
+    
+
+    // create the bullet and listen to the mouse click event
+
+    
     $('.area').append(createball())
-    /*setInterval(function(){
+    setInterval(function(){
         $('.area').append(createball());
 
-    },200);*/
-    var ball = $('.ball');
-    function delBall(){
-        if ($('.ball').css('top') === '100vh') {
-            console.log("11");
-            $('.ball').remove();
-        }
-    }
-    delBall();
+    },200);
+
+    createbullet();
+
+
 
 
 });
